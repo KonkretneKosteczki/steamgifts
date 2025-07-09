90d932d4c1674acfe3c70c025081ccfecb29ebca
Krzysztofadam <krzysztofadamkrzeszewski@gmail.com>
2025-07-09 23:59:55 +0200

    fix: allow unknown fields in giveaway entry responses

--- src/services/steam-gifts/validators/giveaway-entry-responses.validator.ts
+++ src/services/steam-gifts/validators/giveaway-entry-responses.validator.ts
@@ -6,7 +6,7 @@ const entrySuccessfulSchema = Joi.object<IEntrySuccessful>({
     entry_count: Joi.string(),
     points: Joi.string(),
     type: Joi.string()
-});
+}).unknown(true).strip(true);
 
 export function instanceOfIEntrySuccessful(data: unknown): data is IEntrySuccessful {
     const validationResult = entrySuccessfulSchema.validate(data);
@@ -14,7 +14,9 @@ export function instanceOfIEntrySuccessful(data: unknown): data is IEntrySuccess
     return !validationResult.error;
 }
 
-const entryFailedSchema = Joi.object<IEntryFailed>({msg: Joi.string()});
+const entryFailedSchema = Joi.object<IEntryFailed>({
+    msg: Joi.string()
+}).unknown(true).strip(true);
 
 export function instanceOfIEntryFailed(data: unknown): data is IEntryFailed {
     const validationResult = entryFailedSchema.validate(data);
